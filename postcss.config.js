6cede18af2e1a6dc453bc02ba4da222ade91e393
Sam Strong <sam.strong@myunidays.com>
2020-01-29 09:28:55 +0000

    Attempting to get Post CSS import working.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-      require('postcss-nested')
+      require('postcss-import') ,
+      require('tailwindcss')//,
+      // require('autoprefixer'),
+      // require('postcss-nested')
     ]
   }
